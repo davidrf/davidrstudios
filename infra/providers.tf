68460084c74f28d8c4d9258b2d150e7030317b70
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2025-07-20 12:51:26 -1000

    initialize new TF state

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "davidrstudios-terraform-state"
+    key            = "terraform/davidrstudios.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "davidrstudios-terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
