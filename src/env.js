53b1f8490fe3a106ee7e5bd7763d526bd8791d9b
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2025-07-19 20:14:14 -1000

    Fix typo

--- src/env.js
+++ src/env.js
@@ -38,7 +38,7 @@ export const env = createEnv({
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_URL: process.env.NEXT_AUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
