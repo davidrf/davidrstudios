0c7a983b84726a6804b86157ed56ac3f338dfe58
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2025-07-24 18:17:00 -1000

    chore: read site url from env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL,
   vite: {
     plugins: [tailwindcss()]
   }
